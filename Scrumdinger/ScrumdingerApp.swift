fba99a0c646930c6f2c994f95f6fc754787975a0
Yohaku Shiro <yohaku.shiro.22@gmail.com>
2023-02-13 10:47:22 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yohaku on 2023/02/13.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
