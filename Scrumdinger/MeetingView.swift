2fc107f005d94507b2c413b40d12a0bb050e7620
Yohaku Shiro <yohaku.shiro.22@gmail.com>
2023-02-13 15:05:16 +0900

    Supplement accessibility data

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -17,6 +17,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel("Time remaining")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 24)
             HStack {
@@ -25,6 +28,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next speaker")
             }
         }
         .padding()
