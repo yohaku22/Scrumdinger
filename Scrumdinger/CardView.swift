55b461bd58de1c6b88d96e72c5b41c07fcb6d2c0
Yohaku Shiro <yohaku.shiro.22@gmail.com>
2023-02-14 18:28:02 +0900

    Make the card view accessible

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -6,12 +6,14 @@ struct CardView: View {
         VStack(alignment: .leading) {
             Text(scrum.title)
                 .font(.headline)
+                .accessibilityAddTraits(.isHeader)
             Spacer()
             HStack {
                 Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityLabel("\(scrum.attendees.count) attendees")
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
-                    .padding(.trailing, 20)
+                    .accessibilityLabel("\(scrum.lengthInMinutes) minutes meeting")
                     .labelStyle(.trailingIcon)
 
             }
