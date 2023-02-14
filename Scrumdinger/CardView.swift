9e0773dc8d09a9eb228fd5fe2009b14a8c4ce474
Yohaku Shiro <yohaku.shiro.22@gmail.com>
2023-02-14 18:25:32 +0900

    Customize the label style

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -12,6 +12,7 @@ struct CardView: View {
                 Spacer()
                 Label("\(scrum.lengthInMinutes)", systemImage: "clock")
                     .padding(.trailing, 20)
+                    .labelStyle(.trailingIcon)
 
             }
             .font(.caption)
